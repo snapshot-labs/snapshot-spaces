e7a6fd638df4d8acf4c3eb70ac2f1f12944e3136
Chaitanya <yourchaitu@gmail.com>
2024-04-08 21:28:26 +0530

    Update scripts/newAlias.ts
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>

--- scripts/newAlias.ts
+++ scripts/newAlias.ts
@@ -5,7 +5,12 @@ const args = process.argv.slice(2);
 if (args.length === 2) {
   const oldSpace = args[0];
   const newSpace = args[1];
-  if (!aliases[oldSpace]) aliases[oldSpace] = newSpace
+  if (aliases[oldSpace]) {
+    console.error('Alias already exists for ' + oldSpace)
+    process.exit(1);
+  }
+  
+  aliases[oldSpace] = newSpace
   require('fs').writeFileSync('spaces/aliases.json', JSON.stringify(aliases, null, 2), 'utf8');
 } else {
   console.log('Usage: node scripts/newAlias.ts <oldSpace> <newSpace>');
