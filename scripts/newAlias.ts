751bcd80530f9060c2f1e32291fd32c0bf75156f
ChaituVR <yourchaitu@gmail.com>
2024-04-08 11:28:08 +0530

    Add script file

---
+++ scripts/newAlias.ts
@@ -0,0 +1,13 @@
+const aliases = require('../spaces/aliases.json');
+const https = require('https');
+
+const args = process.argv.slice(2);
+if (args.length === 2) {
+  const oldSpace = args[0];
+  const newSpace = args[1];
+  if (!aliases[oldSpace]) aliases[oldSpace] = newSpace
+  // update rpcs.json file with new rpc
+  require('fs').writeFileSync('spaces/aliases.json', JSON.stringify(aliases, null, 2), 'utf8');
+} else {
+  console.log('Usage: node scripts/newAlias.ts <oldSpace> <newSpace>');
+}
